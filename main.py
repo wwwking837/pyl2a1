4b00bfcf5b2993c67ae2748adc1884c7a84007e4
wwwking837 <muhammadzaroonadnan@gmail.com>
2025-02-10 18:42:48 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+name = "shivangi"
+age = 25
+is_student = True
+weight = 38.5
+print("Name :", name)
+print("Data Type of Age is", type(name))
+
+print("Age :", age)
+print("Data Type of age is", type(age))
+
+print("is student :", is_student)
+print("Data Type of is_student is", type(is_student))
+
+print("Weight :", weight)
+print("Data Type of weight is", type(weight))
